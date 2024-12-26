e133ec2c38e3fa654855dd70241a62f63db78be4
christian ycaro <a.christianycaro@gmail.com>
2024-12-25 21:12:26 -0800

    create a function that removes elements from an array

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(array) {
+    let args = Array.from(arguments).slice(1);
+    //creates array from arguments starting after the initial array argument
+    for(let i = array.length - 1; i >= 0; i--) {
+        //looping backwards through the array prevents issues with skipping over array elements
+        for(const piece of args) {
+            //from mdn looping lesson
+            if (piece === array[i]) {
+                array.splice(i, 1);
+            }
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
