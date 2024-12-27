cc90f86252a534fd96c46e891156db352d26aa00
christian ycaro <a.christianycaro@gmail.com>
2024-12-26 16:24:36 -0800

    create system to determine if a year is a leap year

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,14 @@
-const leapYears = function() {
+const leapYears = function(year) {
 
+    const divFour = year % 4 === 0;
+    const divHun = year % 100 === 0;
+    const divFourHun = year % 400 === 0;
+    
+    if (divFour && (!divHun || divFourHun)) {
+        return true
+    } else {
+        return false
+    }
 };
 
 // Do not edit below this line
